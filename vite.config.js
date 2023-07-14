faad2d7b7180da721ab6e7d0542c1cdb97a46de1
dricky23 <138646756+dricky23@users.noreply.github.com>
2023-07-14 16:25:37 -0500

    test2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 // vite.config.js
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import { cssInjectedByJsPlugin } from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-  build: {
+	plugins: [cssInjectedByJsPlugin()],
+build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'login.html'),
+        login: resolve(__dirname, 'login.html'),
+      },
+	  output: {
+        manualChunks: undefined,
       },
     },
   },
