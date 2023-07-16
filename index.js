2bebe232fccac6bc2d645a8ee24a6757f24418a5
dricky23 <138646756+dricky23@users.noreply.github.com>
2023-07-15 21:21:06 -0500

    grrr

--- index.js
+++ index.js
@@ -92,6 +92,61 @@ createUserWithEmailAndPassword(auth, email, password,)
     <ul id="shopping-list">
     </ul>
     </div>`;
+
+    const inputFieldEl = document.getElementById("input-field")
+    const addButtonEl = document.getElementById("add-button")
+    const shoppingListEl = document.getElementById("shopping-list")
+
+    addButtonEl.addEventListener("click", function() {
+      let inputValue = inputFieldEl.value
+      
+      push(shoppingListInDB, inputValue)
+      
+      clearInputFieldEl()
+    })
+    
+    onValue(shoppingListInDB, function(snapshot) {
+      if (snapshot.exists()) {
+          let itemsArray = Object.entries(snapshot.val())
+      
+          clearShoppingListEl()
+          
+          for (let i = 0; i < itemsArray.length; i++) {
+              let currentItem = itemsArray[i]
+              let currentItemID = currentItem[0]
+              let currentItemValue = currentItem[1]
+              
+              appendItemToShoppingListEl(currentItem)
+          }    
+      } else {
+          shoppingListEl.innerHTML = "No items here... yet"
+      }
+    })
+    
+    function clearShoppingListEl() {
+      shoppingListEl.innerHTML = ""
+    }
+    
+    function clearInputFieldEl() {
+      inputFieldEl.value = ""
+    }
+    
+    function appendItemToShoppingListEl(item) {
+      let itemID = item[0]
+      let itemValue = item[1]
+      
+      let newEl = document.createElement("li")
+      
+      newEl.textContent = itemValue
+      
+      newEl.addEventListener("dblclick", function() {
+          let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+          
+          remove(exactLocationOfItemInDB)
+      })
+      
+      shoppingListEl.append(newEl)
+    };
     // ...
   } else {
     // User is signed out
@@ -112,53 +167,3 @@ logout.addEventListener("click", (e)=> {
 });
 });
 
-addButtonEl.addEventListener("click", function() {
-  let inputValue = inputFieldEl.value
-  
-  push(shoppingListInDB, inputValue)
-  
-  clearInputFieldEl()
-})
-
-onValue(shoppingListInDB, function(snapshot) {
-  if (snapshot.exists()) {
-      let itemsArray = Object.entries(snapshot.val())
-  
-      clearShoppingListEl()
-      
-      for (let i = 0; i < itemsArray.length; i++) {
-          let currentItem = itemsArray[i]
-          let currentItemID = currentItem[0]
-          let currentItemValue = currentItem[1]
-          
-          appendItemToShoppingListEl(currentItem)
-      }    
-  } else {
-      shoppingListEl.innerHTML = "No items here... yet"
-  }
-})
-
-function clearShoppingListEl() {
-  shoppingListEl.innerHTML = ""
-}
-
-function clearInputFieldEl() {
-  inputFieldEl.value = ""
-}
-
-function appendItemToShoppingListEl(item) {
-  let itemID = item[0]
-  let itemValue = item[1]
-  
-  let newEl = document.createElement("li")
-  
-  newEl.textContent = itemValue
-  
-  newEl.addEventListener("dblclick", function() {
-      let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
-      
-      remove(exactLocationOfItemInDB)
-  })
-  
-  shoppingListEl.append(newEl)
-};
